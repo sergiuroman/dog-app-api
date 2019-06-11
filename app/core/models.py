10a1a05b45b9a191cae7582a3d9bd2993a5bf8d5
sergiu.97 <thisis.sergiu@gmail.com>
2019-06-11 18:38:25 +0300

    some changes

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         if not email:
@@ -19,14 +18,17 @@ class UserManager(BaseUserManager):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     objects = UserManager()
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,9 +9,9 @@ class ModelTests(TestCase):
         email = 'test159@gmail.com'
         password = 'pass123'
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+              email=email,
+              password=password
+              )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
